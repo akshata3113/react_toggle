19dd5fc9be7f5518155f33bd8a32398b1049180e
akshata3113 <akshatashejwal2004@gmail.com>
2025-07-17 23:14:21 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  const [display, setDisplay] = useState(true)
+
+  return (
+    <>
+      <h1>Toggle in react</h1>
+      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+
+{
+  display?  <h1>User Akshata</h1>:null
+}
+     
+    </>
+  )
+}
+
+export default App
+//akshata
